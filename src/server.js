2459d59177eae0084d1b2f3cc6119cf7ea1a8e0e
GilsonOliveira <juninhokaponnemg@hotmail.com>
2022-01-16 23:03:35 -0300

    added cors to resolve local conflicts

--- src/server.js
+++ src/server.js
@@ -3,11 +3,13 @@ const mongoose = require('mongoose');
 require('dotenv').config(); 
 const routes = require('./routes');
 const connectToDatabase = require('./database.js');
+const cors = require('cors');
 const port = 3333;
 const app = express();
 
 connectToDatabase();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
